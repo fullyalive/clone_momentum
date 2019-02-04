1d9c382ad4ecb0bce4fcc38dcc540196093ea6d1
fullyalive <wogud1202@gmail.com>
2019-02-04 11:33:38 +0700

    JS Clock 생성

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clockContainer = document.querySelector(".js-clock"); // querySelector는 element의 자식을 탐색한다. 이 경우는 js-clock의 자식을 탐색하고 싶은 것
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } : ${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
