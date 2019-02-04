45640b5065bd9c1f9c375d756eec632aef7a2f69
fullyalive <wogud1202@gmail.com>
2019-02-04 17:36:29 +0700

    배경에 랜덤 이미지 호출

---
+++ background.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 6;
+
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `images/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image)
+}
+
+function genRandom() {
+  // 랜덤 넘버 생성
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+
+init();
